ab0ba586ba1e9f038aaebda6000ef2c4700020c7
Thomas S <syntavian@gmail.com>
2025-09-13 21:23:03 +1000

    implement deploy script os detection

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,27 @@
 
 script_dir=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
-cp "${script_dir}/settings.json" "${HOME}\AppData\Roaming\Code\User\settings.json"
+if [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
+    os="win"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    os="mac"
+else
+    echo "Unknown OS detected: $OSTYPE" >&2
+    exit 1
+fi
+
+case $os in
+win)
+    target_file="$HOME/AppData/Roaming/Code/User/settings.json"
+    ;;
+mac)
+    target_file="$HOME/Library/Application Support/Code/User/settings.json"
+    ;;
+*)
+    echo "Unimplemented OS target: $os" >&2
+    exit 1
+    ;;
+esac
+
+echo "Updating VS Code settings file: $target_file"
+cp "${script_dir}/settings.json" "$target_file"
